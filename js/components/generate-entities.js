14513fb5791f9a1c552ce967761d808d44e5b3cb
Sorskoot <mental_c@hotmail.com>
2021-02-21 23:03:15 +0100

    add stone, twig and grass

---
+++ js/components/generate-entities.js
@@ -0,0 +1,13 @@
+WL.registerComponent('generate-entities', {
+ 
+}, {
+    init: function() {
+        console.log('init() with param', this.param);
+    },
+    start: function() {
+        console.log('start() with param', this.param);
+    },
+    update: function(dt) {
+        console.log('update() with delta time', dt);
+    },
+});
